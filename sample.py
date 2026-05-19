6ec2e97094af4ca92c1771ef74e45a4dbb5a7d0f
Timothy Paendong <paendongtimothy7@gmail.com>
2026-05-19 21:09:13 +0700

    add ckpt (ema_model and model) to main, fixed noise for sampling

--- sample.py
+++ sample.py
@@ -10,16 +10,19 @@ def sample_euler(
     model,
     cfg,
     n_samples: int,
-    num_steps=100, 
+    num_steps: int,
+    fixed_noise = None 
 ):
-    model.eval()
+    if fixed_noise is not None:
+        x = fixed_noise.to(cfg.device)
+    else:
+        x = torch.randn(n_samples, cfg.channels, 28, 28, device=cfg.device)
 
-    x = torch.randn(n_samples, cfg.channels, 28, 28, device=cfg.device)
     t = torch.linspace(0, 1, steps=num_steps, device=cfg.device)
     dt = 1 / num_steps
 
     for i in range(num_steps):
-        t_val = torch.full((n_samples,), t[i], device=cfg.device)
+        t_val = torch.full((x.shape[0],), t[i], device=cfg.device)
         x = x + dt * model(x, t_val)
     
     return x
@@ -42,14 +45,21 @@ def main():
 
     parser = argparse.ArgumentParser(description='Sampling Configuration')
     parser.add_argument('-cp', '--ckpt_path', type=str, required=True, metavar='', help='path to checkpoint file')
+    parser.add_argument('-md', '--model', type=str, required=True, metavar='', help='which model for sampling (temporary for learning)')
     parser.add_argument('-op', '--output_path', type=str, metavar='', help='path to save sampled image')
-    parser.add_argument('-ns', '--n_samples', type=int, metavar='', help='n sample image to save', default=3)
+    parser.add_argument('-ns', '--n_samples', type=int, metavar='', default=3, help='n sample image to save')
+    parser.add_argument('-ss', '--sampling_steps', type=int, metavar='', default=100, help='n steps for sampling')
+    parser.add_argument('-fn', '--fixed_noise', type=str, metavar='', default=None, help='path for fixed noise tensor')
     args = parser.parse_args()
 
-    model = Unet(cfg).to(cfg.device)
-    model.load_state_dict(torch.load(args.ckpt_path, map_location=cfg.device))
+    noise = torch.load(args.fixed_noise) if args.fixed_noise else None
+    ckpt = torch.load(args.ckpt_path, map_location=cfg.device)
 
-    images = sample_euler(model, cfg, n_samples=args.n_samples)
+    model = Unet(cfg).to(cfg.device)
+    model.load_state_dict(ckpt[args.model])
+    model.eval()
+
+    images = sample_euler(model, cfg, n_samples=args.n_samples, num_steps=args.sampling_steps, fixed_noise=noise)
     save_samples(images, args.output_path)
 
 if __name__ == '__main__':
