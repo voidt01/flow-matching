9771f70d11e23032d2f13deff12c2be838929ef9
Timothy Paendong <paendongtimothy7@gmail.com>
2026-05-21 21:16:28 +0700

    fix ema initialization (lazily intialized) after n update steps

--- utils.py
+++ utils.py
@@ -8,19 +8,27 @@ class EMA:
         self.beta = beta
         self.update_after_step = update_after_step
         self.model = model
-        self.ema_model = copy.deepcopy(model).eval()
+        self.ema_model = None
 
-        for p in self.ema_model.parameters():
-            p.requires_grad_(False)
+    def init_after_step(self, step):
+        if step == self.update_after_step:
+            self.ema_model = copy.deepcopy(self.model).eval()
+            for p in self.ema_model.parameters():
+                p.requires_grad_(False)
 
     @torch.no_grad()
     def update(self, step):
         if step < self.update_after_step:
             return
+        
+        self.init_after_step(step)
+
         for m_param, ema_param in zip(self.model.parameters(), self.ema_model.parameters()):
             ema_param.mul_(self.beta).add_(m_param * (1 - self.beta))
     
     def state_dict(self):
+        if self.ema_model is None:
+            return None
         return self.ema_model.state_dict()
     
 def set_seed(seed: int):
